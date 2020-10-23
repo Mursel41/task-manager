b86e539060ebf876563d2b6e5b959a4a21ef244d
Mursel Genc <murselgenc41@gmail.com>
2020-10-24 00:38:44 +0200

    Updates

---
+++ precache-manifest.f42754c1d3d2838c401de6732c33088e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf6a383845628997d70fa6c08431e79",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "b97b32a5f9e68bf69139",
+    "url": "/task-manager/static/css/main.93f5fe7f.chunk.css"
+  },
+  {
+    "revision": "94951c93dc77174360e2",
+    "url": "/task-manager/static/js/2.66444f77.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/task-manager/static/js/2.66444f77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97b32a5f9e68bf69139",
+    "url": "/task-manager/static/js/main.43ad90a8.chunk.js"
+  },
+  {
+    "revision": "6f879587e75cb206c02c",
+    "url": "/task-manager/static/js/runtime-main.97270499.js"
+  }
+]);
